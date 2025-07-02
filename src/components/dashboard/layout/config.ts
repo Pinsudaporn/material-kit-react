8cccb51f716c24795fe8d927892eab79bf21d046
pinsudaporn <pinsudaporn123@gmail.com>
2025-07-02 22:12:05 +0700

    //uppp

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -2,7 +2,7 @@ import type { NavItemConfig } from '@/types/nav';
 import { paths } from '@/paths';
 
 export const navItems = [
-  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
+  // { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
   { key: 'resume', title: 'Cv-Resume', href: paths.dashboard.resume, icon: 'user' },
   // { key: 'customers', title: 'Customers', href: paths.dashboard.customers, icon: 'users' },
   // { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
